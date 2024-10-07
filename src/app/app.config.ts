ea12b616666cd62cd00dd5b2ed5ef60266ff90cf
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-10-07 17:08:57 -0500

    configuracion entorno de desarrollo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {provideHttpClient, withInterceptors, withFetch,} from '@angular/common/http'; //para hacer el llamado a la Api
+// import {injectSessionInterceptor} from './core/interceptors/inject-session.interceptor'
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes),
+      provideClientHydration(),
+      provideAnimationsAsync(),
+      provideHttpClient(withInterceptors([]), withFetch())
+    ]
 };
