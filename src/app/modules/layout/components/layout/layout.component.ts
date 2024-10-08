054128f174128128e77a5bddbcd8a0e713f15aa3
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-10-08 16:16:01 -0500

    agregamos el componente not-found y actualizamos algunas carpetas

--- src/app/modules/layout/components/layout/layout.component.ts
+++ src/app/modules/layout/components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Sidebar01Component} from '../../../../shared/components/sidebar01/sidebar01.component'
+import {Sidebar01Component} from '../../../../shared/components/sidebar01/sidebar01.component';
+import {SidebarComponent} from '../../../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [Sidebar01Component, RouterOutlet],
+  imports: [Sidebar01Component, RouterOutlet, SidebarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
--- src/app/shared/components/not-found/not-found.component.ts
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLinkWithHref} from '@angular/router' // esto nos sirve para que cuando nos devolvamos del a pagina 404 no vuelva a recargar la pagina
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkWithHref, CommonModule, MatCardModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
