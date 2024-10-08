a1b7c3df255550af4880927f42b82b3d80d2702f
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-10-08 16:20:52 -0500

    configuramos los guardines para el acceso de rutas dependiedo de los roles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './modules/layout/components/layout/layout.component';
+import {authGuard} from './core/guards/auth.guard';
+import {redirectGuard} from './core/guards/redirect.guard';
 
 export const routes: Routes = [
-    // {
-    //     path:'',//ruta principal
-    //     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule)
-    // },
+    {
+        path:'',//ruta principal
+        canActivate: [redirectGuard],
+        loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule)
+    },
     {
         path:'',
+        canActivate: [authGuard],
         component:LayoutComponent,
         loadChildren: () => import ('./modules/layout/layout.routes').then((m) => m.layoutRoutes)
     }
--- src/app/modules/layout/layout.routes.ts
+++ src/app/modules/layout/layout.routes.ts
@@ -1,30 +1,44 @@
 import { Routes } from "@angular/router";
+import { authGuard } from "../../core/guards/auth.guard";
+import { roleGuard } from "../../core/guards/role.guard";
 import { NotFoundComponent } from "../../shared/components/not-found/not-found.component";
 
 export const layoutRoutes: Routes = [
     {
         path: 'dashboard',
         loadChildren: () => import('../dashboard/dashboard.routes').then(m => m.dashboardRouter),
+        canActivate: [authGuard, roleGuard], // Se requiere autenticación y rol específico
+        data: { expectedRole: 'Administrador' } // Solo el Administrador puede acceder
     },
     {
         path: 'usuarios',
         loadChildren: () => import('../usuarios/usuario.routes').then(m => m.usuarioRouter),
+        canActivate: [authGuard, roleGuard],
+        data: { expectedRole: 'Administrador' }
     },
     {
         path: 'medicamentos',
         loadChildren: () => import('../medicamentos/medicamento.routes').then(m => m.medicamentoRouter),
+        canActivate: [authGuard, roleGuard],
+        data: { expectedRole: 'Administrador' }
     },
     {
         path: 'venta',
         loadChildren: () => import('../venta/venta.routes').then(m => m.ventaRouter),
+        canActivate: [authGuard, roleGuard],
+        data: { expectedRole: ['Empleado', 'Administrador'] } // Tanto Vendedor como Administrador pueden acceder
     },
     {
         path: 'historialVenta',
         loadChildren: () => import('../historialVenta/historialVenta.routes').then(m => m.historialventaRouter),
+        canActivate: [authGuard, roleGuard],
+        data: { expectedRole: ['Empleado', 'Administrador'] } // Tanto Vendedor como Administrador pueden acceder
     },
     {
         path: 'reportes',
         loadChildren: () => import('../reportes/reporte.routes').then(m => m.reporteRouter),
+        canActivate: [authGuard, roleGuard],
+        data: { expectedRole: 'Administrador' }
     },
     {
         path: '401 No autorizado', // Ruta para la página 404
