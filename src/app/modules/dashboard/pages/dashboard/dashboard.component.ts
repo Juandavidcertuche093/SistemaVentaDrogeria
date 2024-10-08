1f8717e2925024bfb99017d79930e77f4f0513d9
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-10-08 14:37:25 -0500

    se crea el compoenente del dashboard con sus funcionalidades

--- src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//Angular Material
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+//Mostrar gráficos.
+import { Chart, registerables } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+import { DashboardService } from '../../services/dashboard.service';
+import { CommonModule } from '@angular/common';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatGridListModule, MatIconModule, BaseChartDirective, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  totalIngresos:string='0';
+  totalVentas:string='0';
+  totalProductos:string='0'
+
+  constructor(
+    private dashboardServicio: DashboardService
+  ){}
+
+  mostrarGrafico(labelGrafico:any[], dataGrafico:any[]){
+    const chartBarras = new Chart('chartBarras',{ //chartBarras es el id para el camvas
+      type:'bar',
+      data:{
+        labels:labelGrafico,
+        datasets:[{
+          label:'# de Ventas',
+          data:dataGrafico,
+          backgroundColor:[
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)', 
+            'rgba(255, 159, 64, 0.2)', 
+            'rgba(75, 192, 192, 0.2)',
+          ],
+          borderColor:[
+            'rgba(54, 162, 235, 1)',
+            'rgba(153, 102, 255, 1)', 
+            'rgba(255, 159, 64, 1)', 
+            'rgba(75, 192, 192, 1)'
+          ],
+          borderWidth:1
+        }]
+      },
+      options:{
+        maintainAspectRatio:false,
+        responsive:true,
+        scales:{
+          y:{
+            beginAtZero:true
+          }
+        }
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.dashboardServicio.resumen()
+    .subscribe({
+      next:(data) => {
+        if (data.status) {
+          this.totalIngresos = data.value.totalIngresos;
+          this.totalVentas = data.value.totalVentas;
+          this.totalProductos = data.value.totalProductos;
+          
+          const arrayData: any[] = data.value.ventasUltimaSemana
+          const labelTemp = arrayData.map((value) => value.fecha);
+          const dataTemp = arrayData.map((value) => value.total);
+          this.mostrarGrafico(labelTemp,dataTemp);
+        }
+      },
+      error:(e) => {}
+    })
+  }
 
 }
--- src/app/modules/dashboard/services/dashboard.service.ts
+++ src/app/modules/dashboard/services/dashboard.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, tap } from 'rxjs';
+import { environment } from '../../../../environments/environments';
+import { ResponseApi } from '../../../core/models/response-api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
+  private urlApi:string = environment.API_URL + "DashBoard/";
+
+  private http = inject(HttpClient)
+
   constructor() { }
+
+  resumen():Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlApi}Resumen`)
+  }
 }
